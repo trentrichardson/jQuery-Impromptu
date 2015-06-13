be3e86a6e1ff83765c2b9469c7b1b7588d1e853c
Trent Richardson <trentdrichardson@gmail.com>
2015-06-12 20:04:12 -0400

    Pull #62 - Allow state position option to be a function

--- dist/jquery-impromptu.js
+++ dist/jquery-impromptu.js
@@ -1,4 +1,4 @@
-/*! jQuery-Impromptu - v6.2.1 - 2015-05-10
+/*! jQuery-Impromptu - v6.2.2 - 2015-05-10
 * http://trentrichardson.com/Impromptu
 * Copyright (c) 2015 Trent Richardson; Licensed MIT */
 (function(root, factory) {
@@ -436,14 +436,15 @@
 				arrow = '',
 				title = '',
 				opts = t.options,
+				pos = $.isFunction(stateobj.position) ? stateobj.position() : stateobj.position,
 				$jqistates = t.jqi.find('.'+ opts.prefix +'states'),
 				buttons = [],
 				showHtml,defbtn,k,v,l,i=0;
 
 			stateobj = $.extend({},Imp.defaults.state, {name:statename}, stateobj);
 
-			if(stateobj.position.arrow !== null){
-				arrow = '<div class="'+ opts.prefix + 'arrow '+ opts.prefix + 'arrow'+ stateobj.position.arrow +'"></div>';
+			if($.isPlainObject(pos) && pos.arrow !== null){
+				arrow = '<div class="'+ opts.prefix + 'arrow '+ opts.prefix + 'arrow'+ pos.arrow +'"></div>';
 			}
 			if(stateobj.title && stateobj.title !== ''){
 				title = '<div class="lead '+ opts.prefix + 'title '+ opts.classes.title +'">'+  stateobj.title +'</div>';
@@ -630,7 +631,7 @@
 				restoreFx = $.fx.off,
 				$state = t.getCurrentState(),
 				stateObj = t.options.states[$state.data('jqi-name')],
-				pos = stateObj? stateObj.position : undefined,
+				pos = stateObj ? $.isFunction(stateObj.position) ? stateObj.position() : stateObj.position : undefined,
 				$window = $(window),
 				bodyHeight = document.body.scrollHeight, //$(document.body).outerHeight(true),
 				windowHeight = $(window).height(),
